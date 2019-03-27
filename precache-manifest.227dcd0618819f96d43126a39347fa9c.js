12ea2c15774697050a71c9798bc9ef43c497a7a5
Michał Wlazło <michal3535@poczta.fm>
2019-03-27 16:06:51 +0100

    Updates

---
+++ precache-manifest.227dcd0618819f96d43126a39347fa9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-rr/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-rr/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-rr/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "9347ed20ee693a8d157a",
+    "url": "/app-rr/static/js/runtime~main.fe775957.js"
+  },
+  {
+    "revision": "2624111fe935ee8ca8cd",
+    "url": "/app-rr/static/js/main.667fd509.chunk.js"
+  },
+  {
+    "revision": "30644d9227b8960e3480",
+    "url": "/app-rr/static/js/2.4684eb95.chunk.js"
+  },
+  {
+    "revision": "2624111fe935ee8ca8cd",
+    "url": "/app-rr/static/css/main.6a8697dd.chunk.css"
+  },
+  {
+    "revision": "b87de643ecd9f8bf7f57d595ba342ecd",
+    "url": "/app-rr/index.html"
+  }
+];
